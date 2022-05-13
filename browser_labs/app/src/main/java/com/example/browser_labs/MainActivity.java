5306599aac5d6024500b4d60192306f984cf1dc3
Sofiia Orekhova <sofiya_orekhova@outlook.com>
2022-05-14 00:33:34 +0300

    update 1405 1233 am

--- browser_labs/app/src/main/java/com/example/browser_labs/MainActivity.java
+++ browser_labs/app/src/main/java/com/example/browser_labs/MainActivity.java
@@ -3,73 +3,24 @@ package com.example.browser_labs;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 public class MainActivity extends AppCompatActivity {
-
-    WebView webView;
-    SwipeRefreshLayout swipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        webView = (WebView) findViewById(R.id.webView);// включение поддержки JavaScript
-        webView.getSettings().setJavaScriptEnabled(true);// указание страницы загрузки
-        webView.loadUrl("https://www.google.com/");
-
-        swipe = (SwipeRefreshLayout)findViewById(R.id.swipe);
-        swipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onRefresh() {
-                WebAction();
+            public void onClick(View v) {
+                String url = "http://www.amazon.com";
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
             }
         });
-
-        WebAction();
-
-    }
-
-
-    public void WebAction(){
-
-        webView = (WebView) findViewById(R.id.webView);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setAppCacheEnabled(true);
-        webView.loadUrl("https://www.google.com/");
-        swipe.setRefreshing(true);
-        webView.setWebViewClient(new WebViewClient(){
-
-            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-
-                Uri uriUrl = Uri.parse("https://www.google.com/");
-                //webView.loadUrl("file:///android_assets/error.html");
-                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
-                startActivity(launchBrowser);
-
-            }
-
-            public void onPageFinished(WebView view, String url) {
-                // do your stuff here
-                swipe.setRefreshing(false);
-            }
-
-        });
-
-    }
-
-
-    @Override
-    public void onBackPressed(){
-
-        if (webView.canGoBack()){
-            webView.goBack();
-        }else {
-            finish();
-        }
     }
 }
---
+++ browser_labs/app/src/main/java/com/example/browser_labs/SecondActivity.java
@@ -0,0 +1,75 @@
+package com.example.browser_labs;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+public class SecondActivity extends AppCompatActivity {
+
+    WebView webView;
+    SwipeRefreshLayout swipe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webView);// включение поддержки JavaScript
+        webView.getSettings().setJavaScriptEnabled(true);// указание страницы загрузки
+        webView.loadUrl("https://www.google.com/");
+
+        swipe = (SwipeRefreshLayout)findViewById(R.id.swipe);
+        swipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                WebAction();
+            }
+        });
+
+        WebAction();
+
+    }
+
+
+    public void WebAction(){
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.loadUrl("https://www.google.com/");
+        swipe.setRefreshing(true);
+        webView.setWebViewClient(new WebViewClient(){
+
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+
+                Uri uriUrl = Uri.parse("https://www.google.com/");
+                //webView.loadUrl("file:///android_assets/error.html");
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                startActivity(launchBrowser);
+
+            }
+
+            public void onPageFinished(WebView view, String url) {
+                // do your stuff here
+                swipe.setRefreshing(false);
+            }
+
+        });
+
+    }
+
+
+    @Override
+    public void onBackPressed(){
+
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else {
+            finish();
+        }
+    }
+}
